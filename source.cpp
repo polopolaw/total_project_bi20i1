8fd63425ea9b085146c04f4cba7f7c4124f3ce39
Sergey-ENOT <71493878+Sergey-ENOT@users.noreply.github.com>
2020-11-21 10:45:23 +0600

    add

--- source.cpp
+++ source.cpp
@@ -6,6 +6,8 @@ int main(int argc, char* argv[])
 	setlocale(LC_ALL, "Rus");
 		
 		std::cout << "a + b= " << argv[argc - 1] << argv[1] << argv[2] << std::endl;
+		std::cout << "a + b= "  << argv[1] <<  std::endl;
+		std::cout << "a + b= " << argv[2] << std::endl;
 		std::cout << argc << std::endl;
 		return 0;
 }
